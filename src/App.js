a0e276256d75c8b451bdf94f3a96ac3621efba94
hashir4560 <haashirraja@gmail.com>
2023-10-27 17:25:18 +0500

    Another Example Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./App.css";
-import ConditionalComponent from "./ConditionalComponent";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("products");
+
   return (
     <div className="App">
-      <ConditionalComponent />
+      <div className="header">
+        <div
+          className={`link ${activeTab === "products" ? "active" : null}`}
+          onClick={() => setActiveTab("products")}
+        >
+          Products
+        </div>
+        <div
+          className={`link ${activeTab === "contactus" ? "active" : null}`}
+          onClick={() => setActiveTab("contactus")}
+        >
+          Contact us
+        </div>
+      </div>
+      <div className="content">
+        {activeTab === "products" ? <div>Products page</div> : null}
+        {activeTab === "contactus" ? <div>Contact us page</div> : null}
+      </div>
     </div>
   );
 }
